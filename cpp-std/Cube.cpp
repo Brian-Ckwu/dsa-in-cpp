882c948767e539733ef26cbbed4bca2ba5e62aa3
Brian Wu <sonny40920710@gmail.com>
2023-01-02 17:48:31 +0800

    test(cpp-std): add and test the clang-format hook

--- cpp-std/Cube.cpp
+++ cpp-std/Cube.cpp
@@ -1,6 +1,6 @@
 /**
  * Simple C++ class for representing a Cube.
- * 
+ *
  * @author
  *   Wade Fagen-Ulmschneider <waf@illinois.edu>
  */
@@ -8,15 +8,10 @@
 #include "Cube.h"
 
 namespace uiuc {
-  double Cube::getVolume() {
-    return length_ * length_ * length_;
-  }
+// Test namespace format
+double Cube::getVolume() { return length_ * length_ * length_; }
 
-  double Cube::getSurfaceArea() {
-    return 6 * length_ * length_;
-  }
+double Cube::getSurfaceArea() { return 6 * length_ * length_; }
 
-  void Cube::setLength(double length) {
-    length_ = length;
-  }
-}
+void Cube::setLength(double length) { length_ = length; }
+}  // namespace uiuc
