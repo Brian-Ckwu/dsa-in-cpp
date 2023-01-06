073e6a3518eba3ab5634c6ea8e3851014e86e0e1
Brian Wu <sonny40920710@gmail.com>
2023-01-06 14:35:30 +0800

    challenge(course-1_week-4): complete the in-course Tower of Hanoi challenge

--- cpp-tower/Game.cpp
+++ cpp-tower/Game.cpp
@@ -1,16 +1,17 @@
 /**
  * C++ class for a game of the Tower of Hanoi puzzle.
- * 
+ *
  * @author
  *   Wade Fagen-Ulmschneider <waf@illinois.edu>
  */
 
 #include "Game.h"
+
+#include <iostream>
+
 #include "Stack.h"
 #include "uiuc/Cube.h"
 #include "uiuc/HSLAPixel.h"
-
-#include <iostream>
 using std::cout;
 using std::endl;
 
@@ -18,9 +19,21 @@ using std::endl;
 // (Feel free to call "helper functions" to help you solve the puzzle.)
 void Game::solve() {
   // Prints out the state of the game:
-  cout << *this << endl;
+  move(4, 0, 1);
 
   // @TODO -- Finish solving the game!
+}
+
+void Game::move(int num, int from, int to) {
+  if (num == 1) {
+    Cube top_cube = stacks_[from].removeTop();
+    stacks_[to].push_back(top_cube);
+    cout << *this << endl;
+    return;
+  }
+  move(num - 1, from, 3 - from - to);
+  move(1, from, to);
+  move(num - 1, 3 - from - to, to);
 }
 
 // Default constructor to create the initial state:
@@ -28,7 +41,7 @@ Game::Game() {
   // Create the three empty stacks:
   for (int i = 0; i < 3; i++) {
     Stack stackOfCubes;
-    stacks_.push_back( stackOfCubes );
+    stacks_.push_back(stackOfCubes);
   }
 
   // Create the four cubes, placing each on the [0]th stack:
@@ -45,7 +58,7 @@ Game::Game() {
   stacks_[0].push_back(yellow);
 }
 
-std::ostream& operator<<(std::ostream & os, const Game & game) {
+std::ostream& operator<<(std::ostream& os, const Game& game) {
   for (unsigned i = 0; i < game.stacks_.size(); i++) {
     os << "Stack[" << i << "]: " << game.stacks_[i];
   }
